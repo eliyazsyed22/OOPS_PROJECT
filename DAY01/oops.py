ed3ba3a13ca96e6067ad2eed132ecfb7e7b71e50
seliyaz <eliyaz7134@gmail.com>
2025-04-05 06:02:00 -0700

    oops concept revision started

---
+++ DAY01/oops.py
@@ -0,0 +1,20 @@
+# Understand oops concept
+
+
+class Atm:
+    
+    
+    # constructor
+    def __init__(self,name,age):
+        
+        # attributes
+        self.name = name
+        self.age = age
+        
+        
+    # method
+    def my_name(self):
+        print(f"Hello my name is {self.name}")
+        
+        
+        
